9044943b32f85e8beacc05c3b885b0f31038117d
Chamath Harshana <it21175466@my.sliit.lk>
2023-07-10 12:00:20 +0530

    QR Vender Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gap/buyer/VS(vendor).dart';
 import 'buyer/buyer_home_page_Android_IOS.dart';
 
 void main() {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: VendorSetting(),
+      home: BuyerHomePage2(),
     );
   }
 }
